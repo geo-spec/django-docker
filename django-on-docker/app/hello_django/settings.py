32366cbfa9153afb846eb496e9bbd0054360e4e8
Georgy <shilingeorgiy@yandex.ru>
2020-09-14 21:51:26 +0300

    docker-compose prod

--- django-on-docker/app/hello_django/settings.py
+++ django-on-docker/app/hello_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'upload'
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,7 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
